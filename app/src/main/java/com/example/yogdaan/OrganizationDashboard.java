fa1fe31b0db01d46e4d5be16acf2e021beecd554
ShoibaShaikh <shoiba2005@gmail.com>
2024-01-31 00:10:08 +0530

    bottom navigation

--- app/src/main/java/com/example/yogdaan/OrganizationDashboard.java
+++ app/src/main/java/com/example/yogdaan/OrganizationDashboard.java
@@ -3,12 +3,19 @@ package com.example.yogdaan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class OrganizationDashboard extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization_dashboard);
+
+
     }
+
 }
