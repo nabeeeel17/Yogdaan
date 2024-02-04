bd63b7a6e53c79aba5e4a90e4b8e335ca1250de7
ShoibaShaikh <shoiba2005@gmail.com>
2024-02-05 00:25:21 +0530

    donated category list

---
+++ app/src/main/java/com/example/yogdaan/BooksDonatedOrg.java
@@ -0,0 +1,14 @@
+package com.example.yogdaan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BooksDonatedOrg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_donated_org);
+    }
+}
